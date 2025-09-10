7d398022583efb047b1acc85574081affed8827f
Jawaheryslam <orchidgraphics19@gmail.com>
2025-09-11 02:28:33 +0300

    basic arithmetic operations modification

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,8 @@
 number1 = int(10)
 number2 = int(5)
-sum = number1 + number2 #Addition: results will be 15
+sum_result = number1 + number2 #Addition: results will be 15
 difference = number1 - number2 #Subtraction: result will be 5
 product = number1 * number2 #Multiplication: result will be 50
-print("Addition of " number1 " and " number2 " is " sum)
+print("Addition of " number1 " and " number2 " is " sum_result)
 print("Subtraction of " number1 " and " number2 " is " difference)
 print("Multiplication of " number1 " and " number2 " is " product)
