855dcc2e0f98e6477a44999aa223d2282ab449bc
Jawaheryslam <orchidgraphics19@gmail.com>
2025-09-12 00:36:34 +0300

    Added line codes on simple interest calculator file

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+principal = 1000 #Representing $1000
+rate = 0.05 #Representing 5% annual interest rate
+time = 3 #Representing 3 yaers
+
+interest = principal * rate * time
+
+print("The simple interest is:", interest)
