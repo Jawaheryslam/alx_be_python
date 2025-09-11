10b92d0c107571df35e5d55d57aa72960333247d
Jawaheryslam <orchidgraphics19@gmail.com>
2025-09-12 00:14:49 +0300

    corrected my code in basic arithmetic exercise

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -3,6 +3,6 @@ number2 = int(5)
 sum_result = number1 + number2 #Addition: results will be 15
 difference = number1 - number2 #Subtraction: result will be 5
 product = number1 * number2 #Multiplication: result will be 50
-print("Addition of " number1 " and " number2 " is " sum_result)
-print("Subtraction of " number1 " and " number2 " is " difference)
-print("Multiplication of " number1 " and " number2 " is " product)
+print("Addition of", number1, "and", number2, "is", sum_result)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
