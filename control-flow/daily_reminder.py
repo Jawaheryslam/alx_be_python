224e31ff56e85733a316bc7438444f9983722f46
Jawaheryslam <orchidgraphics19@gmail.com>
2025-09-19 19:11:17 +0300

    modification for the reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -6,9 +6,9 @@ match priority:
     case "high":
         reminder = f"Reminder: '{task}' is a high priority task"
     case "medium":
-        reminder = f"Reminder '{task}' is a medium priority task"
+        reminder = f"Reminder: '{task}' is a medium priority task"
     case "low":
-        reminder = f"Reminder '{task}' is a low priority task"
+        reminder = f"Reminder: '{task}' is a low priority task"
 
 if time_bound == "yes":
     reminder += " that requires immediate attention today!"
