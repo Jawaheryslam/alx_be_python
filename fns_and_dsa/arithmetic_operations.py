6705c7b7ddad15546dbfd583dff9b8ef79d0b127
Jawaheryslam <orchidgraphics19@gmail.com>
2025-09-28 17:26:35 +0300

    defining a function that performs basic arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,20 @@
+def perform_operation(num1: float, num2: float, operation: str):
+
+    if operation == "add":
+        return num1 + num2
+
+    elif operation == "subtract":
+        return num1 - num2
+
+    elif operation == "multiply":
+        return num1 * num2
+
+    elif operation == "divide":
+
+        if num1 == 0 or num2 == 0:
+            return "Cannot divide by zero"
+
+        return num1 / num2
+
+    else:
+        return "Invalid Operation"
