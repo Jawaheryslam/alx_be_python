c1129f0b6f800ae71fefab0434e3a38d9558ecdf
Jawaheryslam <orchidgraphics19@gmail.com>
2025-10-05 21:54:53 +0300

    2nd correction for unit testing codes

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,28 +7,28 @@ class TestSimpleCalculator(unittest.TestCase):
         self.calc = SimpleCalculator()
 
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(10, 5), 15)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(-1, -1), -2)
         self.assertEqual(self.calc.add(0, 5), 5)
 
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 5), 5)
         self.assertEqual(self.calc.subtract(-1, 1), -2)
         self.assertEqual(self.calc.subtract(-1, -1), 0)
         self.assertEqual(self.calc.subtract(5, 0),5)
 
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(10, 5), 50)
         self.assertEqual(self.calc.multiply(-1, 1), -1)
         self.assertEqual(self.calc.multiply(-1, -1), 1)
         self.assertEqual(self.calc.multiply(10, 0), 0)
 
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 5), 2)
         self.assertEqual(self.calc.divide(10, -2), -5)
         self.assertEqual(self.calc.divide(-10, -5), 2)
